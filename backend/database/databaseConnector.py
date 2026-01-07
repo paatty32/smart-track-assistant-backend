1c8d4357ed6c8dc286060c21beb47defe7e9aa44
Patrick <patrick.boadu.boafo@hotmail.de>
2026-01-07 21:34:18 +0100

    feat: database - implementing connection

---
+++ backend/database/databaseConnector.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlmodel import SQLModel
+
+from domain.TrainingPlan import TrainingPlan
+
+DATABASE_URL = "postgresql+psycopg2://user:password@localhost:5432/smartAssistantDb"
+
+def get_enginge():
+    engine = create_engine(DATABASE_URL, echo=True)
+    return engine
+
+def create_tables(engine):
+    SQLModel.metadata.create_all(engine)
