9230b38b2747dde8caf389b0e0a7f3d799e0135d
Patrick <patrick.boadu.boafo@hotmail.de>
2026-01-07 21:34:51 +0100

    feat: domain - implementing model and DTO for DB

---
+++ backend/domain/TrainingPlan.py
@@ -0,0 +1,12 @@
+from datetime import date
+from typing import Optional
+
+from sqlmodel import SQLModel, Field
+
+
+class TrainingPlan(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    datum: date
+    wetter: str
+    aufwaermen: str
+    hauptteil: str
