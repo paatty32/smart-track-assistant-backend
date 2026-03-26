ac6faaf4ca45e4edefde60f6c8d19a447b91e822
Patrick <patrick.boadu.boafo@hotmail.de>
2026-03-26 23:50:13 +0100

    chore: refactoring - renaming methods

--- mcp_utils/mcp_client.py
+++ mcp_utils/mcp_client.py
@@ -1,8 +1,8 @@
 from llama_index.tools.mcp import BasicMCPClient, McpToolSpec
 
-async def get_weather_tools():
-    mcp_weather_client = BasicMCPClient("http://127.0.0.1:8000/mcp")
-    mcp_weather_tools = McpToolSpec(client=mcp_weather_client)
+async def get_smart_track_assistant_tools():
+    mcp_smart_track_client = BasicMCPClient("http://127.0.0.1:8000/mcp")
+    mcp_smart_track_tools = McpToolSpec(client=mcp_smart_track_client)
 
-    tools = await mcp_weather_tools.to_tool_list_async()
+    tools = await mcp_smart_track_tools.to_tool_list_async()
     return tools
